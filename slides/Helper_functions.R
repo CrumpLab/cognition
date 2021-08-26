5d8a9db89ee1abdf75c31bc32c19929829eb64fe
Matt Crump <crumpmj@gmail.com>
2021-08-26 17:19:12 -0400

    finish lecture 2 slides

---
+++ slides/Helper_functions.R
@@ -0,0 +1,21 @@
+items <- c("Beginning",
+           "Middle",
+           "Another middle",
+           "End")
+
+title <- "Roadmap"
+
+create_roadmaps <- function(items, title="Roadmap", use_item_title=FALSE){
+  for(j in 1:length(items)){
+    if(use_item_title==FALSE) cat("## ", title)
+    if(use_item_title) cat("## ", items[j])
+    cat("\n")
+    cat("\n")
+    for(i in 1:length(items)){
+      if(i!=j) cat("### ",i,". ",items[i]," {.greyout}", sep = "")
+      if(i==j) cat("### ",i,". ",items[i]," {.bolder}", sep = "")
+      cat("\n")
+    }
+    cat("\n")
+  }
+}
