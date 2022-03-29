3ff57e3d921b699dd43224b226b8f3e02cee334f
Matt Crump <crumpmj@gmail.com>
2022-03-29 11:11:57 -0400

    push pdf slides

--- slides/Helper_functions.R
+++ slides/Helper_functions.R
@@ -21,3 +21,30 @@ create_roadmaps <- function(items, title="Roadmap", use_item_title=FALSE){
 }
 
 create_roadmaps(items=items, use_item_title=TRUE)
+
+##
+
+reveal_to_pdf <- function(path_to_slides, output_file = "FALSE"){
+  if (output_file == FALSE){
+    split_input <- unlist(strsplit(path_to_slides,"/"))
+    input_name <- split_input[length(split_input)]
+    input_name <- unlist(strsplit(input_name,"\\."))[1]
+    output_file <- paste(c(split_input[-length(split_input)],
+                            paste0(input_name,".pdf")),collapse="/")
+  }
+  run_decktape <- paste("decktape -p 0 -s 1840x1400 --chrome-path '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'",path_to_slides, output_file)
+  system(run_decktape)
+}
+
+reveal_to_pdf(path_to_slides = paste0(getwd(),"/","L1_Instances/L1_Instances.html"))
+reveal_to_pdf(path_to_slides = paste0(getwd(),"/","L2_QALMRI/L2_QALMRI.html"))
+reveal_to_pdf(path_to_slides = paste0(getwd(),"/","L3_Imagery/L3_Imagery.html"))
+reveal_to_pdf(path_to_slides = paste0(getwd(),"/","L4_Eugenics/L4_Eugenics.html"))
+reveal_to_pdf(path_to_slides = paste0(getwd(),"/","L5_IQ/L5_IQ.html"))
+reveal_to_pdf(path_to_slides = paste0(getwd(),"/","L6_Associations/L6_Associations.html"))
+reveal_to_pdf(path_to_slides = paste0(getwd(),"/","L8_Behaviorism/L8_Behaviorism.html"))
+reveal_to_pdf(path_to_slides = paste0(getwd(),"/","L9_Information/L9_Information.html"))
+reveal_to_pdf(path_to_slides = paste0(getwd(),"/","L10_Memory_1/L10_Memory_1.html"))
+reveal_to_pdf(path_to_slides = paste0(getwd(),"/","L11_Memory_2/L11_Memory_2.html"))
+
+
