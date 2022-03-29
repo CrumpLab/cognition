724cf850cc06cf73c28f216e1157911145e1fe8c
Matt Crump <crumpmj@gmail.com>
2022-03-29 08:38:14 -0400

    convert slides to revealjs

--- slides/Helper_functions.R
+++ slides/Helper_functions.R
@@ -1,18 +1,19 @@
-items <- c("Measuring Memory",
-           "Memory Phenomena",
-           "Memory Principles")
+items <- c("What is this course about?",
+           "Course Resources",
+           "Syllabus and Logistics",
+           "Questions")
 
 title <- "Roadmap"
 
 create_roadmaps <- function(items, title="Roadmap", use_item_title=FALSE){
   for(j in 1:length(items)){
-    if(use_item_title==FALSE) cat("## ", title)
-    if(use_item_title) cat("## ", items[j])
+    if(use_item_title==FALSE) cat("## ", items[j])
+    if(use_item_title) cat("# ", title )
     cat("\n")
     cat("\n")
     for(i in 1:length(items)){
-      if(i!=j) cat("### ",i,". ",items[i]," {.greyout}", sep = "")
-      if(i==j) cat("### ",i,". ",items[i]," {.bolder}", sep = "")
+      if(i!=j) cat("::: {.grey_box} \n",i," ",items[i],"\n",":::", sep = "")
+      if(i==j) cat("::: {.pop_box_2} \n",i," ",items[i],"\n",":::", sep = "")
       cat("\n")
     }
     cat("\n")
